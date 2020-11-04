a5418c0e2872e335309cba3ca303a638de9f24ad
mww_al <maww@chenzhu-inst.com>
2020-11-04 15:56:33 +0800

    1：推送

---
+++ TestProject/Test.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Test.xaml 的交互逻辑
+    /// </summary>
+    public partial class Test : UserControl
+    {
+        private DataTable _dt = new DataTable();
+
+        public Test()
+        {
+            InitializeComponent();
+
+            _dt.Columns.Add(new DataColumn("NO", typeof(string)));
+            _dt.Columns.Add(new DataColumn("ParamKey", typeof(string)));
+            _dt.Columns.Add(new DataColumn("ParamValue", typeof(string)));
+
+            for (int i = 0; i < 48; i++)
+            {
+                DataRow dr = _dt.NewRow();
+                dr[0] = i;
+                dr[1] = i.ToString()+i.ToString();
+                dr[2] = i.ToString() + i.ToString()+i.ToString();
+                _dt.Rows.Add(dr);
+            }
+            this.TestDataGrid.ItemsSource = null;
+            this.TestDataGrid.ItemsSource = _dt.DefaultView;
+        }
+    }
+}
